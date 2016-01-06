f421318419feed8f45d19b0f1b86fc28519ffb5f
Frederico Martini <fred@debian>
2016-01-06 20:02:24 -0200

    default DISTRO_NAME trusty caso sistema operacional debian

--- install_java.sh
+++ install_java.sh
@@ -38,8 +38,8 @@ if [ $SO == 'debian' ]; then
 	apt-get remove oracle-java* -y
 
 	#adicionando repository
-	echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu $DISTRO_NAME main" | tee /etc/apt/sources.list.d/webupd8team-java.list
-	echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu $DISTRO_NAME main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
+	echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee /etc/apt/sources.list.d/webupd8team-java.list
+	echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" | tee -a /etc/apt/sources.list.d/webupd8team-java.list
 	apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys EEA14886
 
 	clear
