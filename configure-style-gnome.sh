9deac7bd67e2805be9f1cba72890c98970f02bbf
Frederico Martini <fredmalmeida@gmail.com>
2016-03-13 17:32:16 -0300

    ajustes script zsh e configure-style-gnome.. ok

--- configure-style-gnome.sh
+++ configure-style-gnome.sh
@@ -3,8 +3,21 @@
 DROP_BOX_INSTALLATION='/home/fred/Cloud/Dropbox/Linux/arquivos_de_configuracao/profiles/'
 
 function configure_nautilus(){
+if [ -e $DROP_BOX_INSTALLATION/terminal-settings-12-03-2016_bkp_pc ]; then
     dconf load /org/gnome/desktop/ < $DROP_BOX_INSTALLATION/gnome-desktop-settings 
-
+fi
 }
 
+function configure_terminal(){
+if [ -e $DROP_BOX_INSTALLATION/terminal-settings-12-03-2016_bkp_pc ]; then
+    dconf load /org/gnome/terminal/ < $DROP_BOX_INSTALLATION/terminal-settings-12-03-2016_bkp_pc
+fi
+}
+
+function main(){
 configure_nautilus
+
+configure_terminal
+}
+
+main
--- install_zsh_with_oh_my_sh.sh
+++ install_zsh_with_oh_my_sh.sh
@@ -4,24 +4,47 @@
 #
 
 function main(){
-    #check if zsh installed
-    INSTALLED=`/bin/bash verify_installation.sh zsh`
-    USER=`ls /home/`
-    
-    if [ ! $INSTALLED -eq 0 ]; then #not intalled
-        apt-get update && apt-get install zsh -y
-    fi
-    
-    my_zsh
+#check if zsh installed
+INSTALLED=`/bin/bash verify_installation.sh zsh`
+USER=`ls /home/`
+
+if [ ! $INSTALLED -eq 0 ]; then #not intalled
+    sudo apt-get update && sudo apt-get install zsh -y
+    INSTALLED=$?
+fi
+
+if [ $INSTALLED -eq 0 ]; then #conseguiu instalar
+    my_zsh_root
+    my_zsh_user
+else
+    echo 'Erro na instalação!'
+fi
+
 }
 
-function my_zsh(){
-    #root
-    su - root sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+function my_zsh_root(){
+#root
+clear
+echo "Insert the root password:"
+sleep 2
+#del folders if exist
+sudo rm -rf /root/.zsh*
+sudo rm -rf /root/.oh-my-zsh/
+su - root sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 
-    #normal user
-    su - $USER sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 }
+
+function my_zsh_user(){
+#normal user
+#del folders if exist
+if [ -d "/home/$USER/.oh-my-zsh/" ]; then
+    rm -rf /home/$USER/.zsh*
+    rm -rf /home/$USER/.oh-my-zsh/
+fi
+su - $USER sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+}
+
 
 main
 
