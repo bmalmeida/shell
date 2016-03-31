ba5bd24ead08f71b0ab4efacd49c21946164e2ce
Frederico Martini <fredmalmeida@gmail.com>
2016-03-30 23:27:09 -0300

    refactoring

--- install_vim.sh
+++ install_vim.sh
@@ -6,41 +6,42 @@ USER=`ls /home/`
 HOME_DIR="/home/$USER"
 
 
-#install vim and vim-gnome
-#apt-get install vim vim-gnome -y
+function install_vim(){
+apt-get install vim vim-gnome -y
 
-#install syntax colorized to vim
-
-#configure vim
-function configure_vim(){
-    #clone to home_dir
-        git clone https://github.com/fredericomartini/vimrc.git $HOME_DIR/.vim_runtime
-            #copy to root dir
-                cp -R $HOME_DIR/.vim_runtime /root/
-
-                    #script install home_dir
-                        sh $HOME_DIR/.vim_runtime/install_awesome_vimrc.sh
-
-                            #script install root_dir
-                                sh /root/.vim_runtime/install_awesome_vimrc.sh
-                                 
-    #modify my_configs.vim  
-
-    cat > $HOME_DIR/.vim_runtime/my_configs.vim <<EOF
-    colorscheme peaksea
-    backgroud = light
-   
-EOF
-    #configure root settings
-    cp -R $HOME_DIR/.vim_runtime/my_configs.vim /root/.vim_runtime/
 }
 
-function install_YouCompleteMe(){
+#configure vim
+function configure_vim() {
+#clone to home_dir
+git clone https://github.com/fredericomartini/vimrc.git $HOME_DIR/.vim_runtime
+#copy to root dir
+cp -R $HOME_DIR/.vim_runtime /root/
+
+#script install home_dir
+sh $HOME_DIR/.vim_runtime/install_awesome_vimrc.sh
+
+#script install root_dir
+sh /root/.vim_runtime/install_awesome_vimrc.sh
+
+#modify my_configs.vim  
+
+cat > $HOME_DIR/.vim_runtime/my_configs.vim <<EOF
+colorscheme peaksea
+backgroud = light
+
+EOF
+#configure root settings
+cp -R $HOME_DIR/.vim_runtime/my_configs.vim /root/.vim_runtime/
+
+}
+
+function install_YouCompleteMe() {
 #install dependency
 apt-get install build-essential cmake python-dev
 
 #YouCompleteMe
-#git clone https://github.com/Valloric/YouCompleteMe.git $HOME_DIR/.vim_runtime/sources_non_forked/YouCompleteMe
+git clone https://github.com/Valloric/YouCompleteMe.git $HOME_DIR/.vim_runtime/sources_non_forked/YouCompleteMe
 
 cd $HOME_DIR/.vim_runtime/sources_non_forked/YouCompleteMe/
 
@@ -55,8 +56,12 @@ git submodule update --init --recursive
 
 }
 
-
-
-#configure_vim
-
+function main(){
+install_vim
+configure_vim
 install_YouCompleteMe
+
+}
+
+main
+
