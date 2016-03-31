5f121596cb99f3be494a59b03800f0ad6f4d1039
Frederico Martini <fredmalmeida@gmail.com>
2016-03-30 23:42:48 -0300

    updated script intall oh-my-zsh

--- install_zsh_with_oh_my_sh.sh
+++ install_zsh_with_oh_my_sh.sh
@@ -1,24 +1,51 @@
 #!/bin/bash
+#
+#install and configure zsh with oh-my-zsh
+#
 
-#install and configure zsh with oh-my-sh
+function main(){
+#check if zsh installed
+INSTALLED=`which zsh`
+USER=`ls /home/`
 
-#apt-get install zsh
-
-#verify_installation(){
-#SUCCESS= `which $1`
-#    if ( $SUCCESS -eq 0 ); then
-#        echo 'instalado'
-#    else
-#        echo 'nao instalado'
-#    fi
-#}
-
-#check if program installed
-$INSTALLED=`(/bin/bash verify_installation.sh 'zsh')`
-
-if [ ! "$INSTALLED" -eq 0 ]; then #not intalled
-    echo 'not installed'
-else
-    echo 'instalado'
+if [ ! -n "$INSTALLED" ]; then #not intalled
+    sudo apt-get update && sudo apt-get install zsh -y
+    INSTALLED=$?
 fi
+
+if [ -n "$INSTALLED" ]; then #conseguiu instalar
+    my_zsh_root
+    my_zsh_user
+else
+    echo 'Erro na instalação!'
+fi
+
+}
+
+function my_zsh_root(){
+#root
+clear
+echo "Insert the root password:"
+sleep 2
+#del folders if exist
+sudo rm -rf /root/.zsh*
+sudo rm -rf /root/.oh-my-zsh/
+su - root sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+}
+
+function my_zsh_user(){
+#normal user
+#del folders if exist
+if [ -d "/home/$USER/.oh-my-zsh/" ]; then
+    rm -rf /home/$USER/.zsh*
+    rm -rf /home/$USER/.oh-my-zsh/
+fi
+su - $USER sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+}
+
+
+main
+
 
