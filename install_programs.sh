e50f00c4eb3ae91fc06d34c6a6a14183fe286dd3
Frederico Martini <fredmalmeida@gmail.com>
2016-05-16 17:24:48 -0300

    update script install, added others scripts

--- install_programs.sh
+++ install_programs.sh
@@ -1,27 +1,28 @@
 #!/bin/bash 
+#declarete array programs
+declare -A PROGRAMS
+
 #informacoes sistema
 DISTRO_INFO=$(lsb_release -i) #Distributor ID: Ubuntu
 DISTRO_ID=${DISTRO_INFO#'Distributor ID:'*} #remove Distributor ID:
 DISTRO_ID=$(echo $DISTRO_ID | sed 's/^[ \t]*//') #remove leading spaces
 DISTRO_ID=${DISTRO_ID,,}                #to_lower
 
-
-MSG_SUCCESS=':: Instalado com sucesso :: '
-MSG_ERROR=':: Erro na instalação :: '
-
 INSTALL_DIR='/usr/local/programas'
 ARCH=$(uname -m)
 
 #programs if true, then will be installed
 BASIC_PACKAGES=false
+#Cloud/Browsers
 CHROME=false
 DROP_BOX=false
 DROP_BOX_DATE='2015.10.28'
 MEGA=false
 MEGA_URL='https://mega.nz/linux/MEGAsync/xUbuntu_16.04/amd64/megasync_2.9.1_amd64.deb' #see more https://mega.nz/linux/MEGAsync/
+#Multimedia
 STREMIO=false
 STREMIO_VERSION=3.5.7  #see more http://dl.strem.io/Stremio3.4.5.linux.tar.gz
-SPOTIFY=true
+SPOTIFY=false
 #IDE'S
 NETBEANS_INSTALL=false
 NETBEANS_VERSION=8.0.2 #see more http://www.netbeans.info/downloads/dev.php
@@ -29,8 +30,13 @@ PHP_STORM=false
 PHP_STORM_VERSION=9.0.2 #see more https://www.jetbrains.com/phpstorm/download/
 EVOLUS_PENCIL_INSTALL=false
 EVOLUS_PENCIL_VERSION=2.0.2
-
-
+MYSQL_WORKBENCH=false
+#others with script
+GIT=false
+VIM=false
+DOCKER=false
+ZSH=false
+JAVA=false
 #################################################
 
 function verify_dir() {
@@ -62,17 +68,14 @@ function install_programs() {
 
         cd $INSTALL_DIR 
         gdebi google-chrome.deb --n 
-        INSTALL_SUCCESS=$?
+        if [ $? -eq 0 ]; then
+            PROGRAMS['chrome']=true
+        else
+            PROGRAMS['chrome']=false;
+        fi
         
         #del files
         rm -rf $INSTALL_DIR/google-chrome* > /dev/null 2>&1
-
-        #verifica se  foi instalado com sucesso
-        if [ $INSTALL_SUCCESS -eq 0 ]; then
-            echo "$MSG_SUCCESS Google Chrome"
-        else
-            echo "$MSG_ERROR Google Chrome"
-        fi
     fi
        
     #instalacao drop-box
@@ -91,17 +94,13 @@ function install_programs() {
 
         cd $INSTALL_DIR
         gdebi dropbox.deb --n
-        INSTALL_SUCCESS=$?
-
+        if [ $? -eq 0 ]; then
+            PROGRAMS['dropbox']=true
+        else
+            PROGRAMS['dropbox']=false;
+        fi
         #del files
         rm -rf $INSTALL_DIR/dropbox-*
-
-        #verifica se foi instalado com sucesso
-        if [ $INSTALL_SUCCESS -eq 0 ]; then
-            echo "$MSG_SUCCESS Dropbox"
-        else
-            echo "$MSG_ERROR Dropbox"
-        fi
     fi
 
     #instalacao mega-cz
@@ -115,22 +114,16 @@ function install_programs() {
         cd $INSTALL_DIR
         
         gdebi mega.deb --n
-        INSTALL_SUCCESS=$?
-
-        rm -rf $INSTALL_DIR/mega-*
-
-        #verifica se foi instalado com sucesso
-        if [ $INSTALL_SUCCESS -eq 0 ]; then
-            echo "$MSG_SUCCESS Mega"
+        if [ $? -eq 0 ]; then
+            PROGRAMS['mega']=true
         else
-            echo "$MSG_ERROR Mega"
+            PROGRAMS['mega']=false;
         fi
+        rm -rf $INSTALL_DIR/mega-*
     fi
 
     #instalacao stremio
     if [ $STREMIO == true ]; then
-        echo 'stremio'
-        exit
         wget http://dl.strem.io/Stremio$STREMIO_VERSION.linux.tar.gz -O $INSTALL_DIR/stremio.tar.gz
         wget http://www.strem.io/3.0/stremio-white-small.png -O $INSTALL_DIR/stremio.png
 
@@ -152,16 +145,12 @@ function install_programs() {
         #cria links
         sudo ln -s $INSTALL_DIR/stremio/Stremio.sh /usr/bin/stremio
         sudo ln -s $INSTALL_DIR/stremio/Stremio-runtime /usr/bin/Stremio-runtime
-        INSTALL_SUCCESS=$?
-
-        rm -rf $INSTALL_DIR/stremio.tar.gz
-
-        #verifica se foi instalado com sucesso
-        if [ $INSTALL_SUCCESS -eq 0 ]; then
-            echo "$MSG_SUCCESS Stremio"
+        if [ $? -eq 0 ]; then
+            PROGRAMS['stremio']=true
         else
-            echo "$MSG_ERROR Stremio"
+            PROGRAMS['stremio']=false;
         fi
+        rm -rf $INSTALL_DIR/stremio.tar.gz
     fi
 
     #instalacao spotify
@@ -186,13 +175,10 @@ function install_programs() {
         apt-get update  -y
 
         apt-get install spotify-client -y 
-        INSTALL_SUCCESS=$?
-
-        #verifica se foi instalado com sucesso
-        if [ $INSTALL_SUCCESS -eq 0 ]; then
-            echo "$MSG_SUCCESS Spotify"
+        if [ $? -eq 0 ]; then
+            PROGRAMS['spotify']=true
         else
-            echo "$MSG_ERROR Spotify"
+            PROGRAMS['spotify']=false;
         fi
     fi
 
@@ -211,25 +197,121 @@ function install_programs() {
 
         chmod +x $INSTALL_DIR/phpStorm/*/bin/phpstorm.sh
         sudo ln -s $INSTALL_DIR/phpStorm/*/bin/phpstorm.sh /usr/bin/phpstorm
-        INSTALL_SUCCESS=$?
-
-        rm -rf $INSTALL_DIR/phpstorm.tar.gz
-
-        #verifica se foi instalado com sucesso
-        if [ $INSTALL_SUCCESS -eq 0 ]; then
-            echo "$MSG_SUCCESS phpStorm"
+        if [ $? -eq 0 ]; then
+            PROGRAMS['phpstorm']=true
         else
-            echo "$MSG_ERROR phpStorm"
+            PROGRAMS['phpstorm']=false;
+        fi
+        rm -rf $INSTALL_DIR/phpstorm.tar.gz
+    fi
+
+    #instalacao mysqlworkbench
+    if [ $MYSQL_WORKBENCH == true ]; then
+#         if [ $ARCH != 'x86_64' ]; then #sistema 32bit
+#             ARCH2=i386
+#         else
+#             ARCH2=amd64
+#         fi  
+# 
+#         #verifica nome distro
+#         if [[ $DISTRO_ID == ubuntu || $DISTRO_ID == debian ]]; then
+#             DISTRO_ID2=1ubu1504
+#         else
+#             DISTRO_ID2=other
+#         fi
+# 
+#         if [ $DISTRO_ID2 != other ]; then
+# 
+#             #wget http://ftp.kaist.ac.kr/mysql/Downloads/MySQLGUITools/mysql-workbench-community-$MYSQL_WORKBENCH_VERSION-$DISTRO_ID2-$ARCH2.deb -O $INSTALL_DIR/mysql-workbench.deb
+# 
+#             #dependencia necessaria libjpeg8
+#             #wget http://ftp.us.debian.org/debian/pool/main/libj/libjpeg8/libjpeg8_8d1-2_$ARCH2.deb -O $INSTALL_DIR/libjpeg8.deb
+# 
+#             sudo apt-get --purge remove mysql* -y > /dev/null 2>&1
+# 
+#             #instalar dependencia
+#             cd $INSTALL_DIR
+#             sudo gdebi libjpeg8.deb  --n  #> /dev/null 2>&1 #auto accept
+#             if [ $? -eq 0 ]; then
+#                 PROGRAMS['mysql-libjpeg8']=true
+#             else
+#                 PROGRAMS['mysql-libjpeg8']=false;
+#             fi
+# 
+#             #dependencia
+#             sudo apt-get install -y libatkmm-1.6*
+#             sudo gdebi mysql-workbench.deb --n # > /dev/null 2>&1 #auto accept
+#             if [ $? -eq 0 ]; then
+#                 PROGRAMS['mysql-workbench']=true
+#             else
+#                 PROGRAMS['mysql-workbench']=false;
+#             fi
+# 
+#             #rm -rf mysql-workbench*
+#             #rm -rf libjpeg8*
+#         else
+#             echo 'Other System.. Please do the download manual
+#             on: http://dev.mysql.com/downloads/workbench/'
+#         fi
+            sudo apt-get install -y mysql-workbench
+            if [ $? -eq 0 ]; then
+                PROGRAMS['phpstorm']=true
+            else
+                PROGRAMS['phpstorm']=false;
+            fi
+    fi
+    
+    #git
+    if [ $GIT == true ]; then
+        sudo ./install_and_configure_git.sh
+    fi
+
+    #vim
+    if [ $VIM == true ]; then
+        sudo ./install_vim.sh
+    fi
+
+    #docker
+    if [ $DOCKER == true ]; then
+        sudo ./install_docker.sh
+        if [ $? -eq 0 ]; then
+            PROGRAMS['docker']=true
+        else
+            PROGRAMS['docker']=false;
+        fi
+    fi
+
+    #zsh
+    if [ $ZSH == true ]; then
+        sudo ./install_zsh_with_oh_my_sh.sh
+    fi
+
+
+    #java
+    if [ $JAVA == true ]; then
+        sudo ./install_java.sh
+        if [ $? -eq 0 ]; then
+            PROGRAMS['java']=true
+        else
+            PROGRAMS['java']=false;
         fi
     fi
 
 
+}
+
+function verify_installation() {
+    clear
+    for index in ${!PROGRAMS[*]}; do
+        echo "Program: $index instalado: ${PROGRAMS[$index]}"
+    done
 
 }
 
 function main() {
     verify_dir
     install_programs
+    verify_installation
 }
 
 main
