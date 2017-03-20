42729f9774412731c5ad32050e0060f373d387f1
Frederico Martini <frederico.almeida@ucpel.edu.br>
2017-03-20 17:38:49 -0300

    updated config sublimetetext3 added themes and config

--- install_sublime_text3.sh
+++ install_sublime_text3.sh
@@ -4,17 +4,17 @@ SUBLIME_DIR=$HOME_DIR/.config/sublime-text-3
 INSTALLED_PACKAGE_DIR=$SUBLIME_DIR/Installed\ Packages
 PACKAGE_DIR=$SUBLIME_DIR/Packages
 #plugins
-#Package Control , SideBarEnhancements, BracketHighlighter, SublimeCodeIntel, Theme - Soda, Alignment, Git, jQuery, DocBlockr, All Autocomplete, GitGutter, AutoFileName, Pretty JSON, Terminal, 
-#Bootstrap 3 Snippets, 
+#Package Control , SideBarEnhancements, BracketHighlighter, SublimeCodeIntel, Theme - Soda, Alignment, Git, jQuery, DocBlockr, All Autocomplete, GitGutter, AutoFileName, Pretty JSON, Terminal,
+#Bootstrap 3 Snippets,
 
 function install() {
-    wget https://download.sublimetext.com/sublime-text_build-3114_amd64.deb -O $HOME_DIR/sublime-text3.deb
-    sudo dpkg --install $HOME_DIR/sublime-text3.deb 
+    wget https://download.sublimetext.com/sublime-text_build-3126_amd64.deb -O $HOME_DIR/sublime-text3.deb
+    sudo dpkg --install $HOME_DIR/sublime-text3.deb
     #copy config
     if [ -e "./sublime-text3/Preferences.sublime-settings" ]; then
         cp -R ./sublime-text3/Preferences.sublime-settings $PACKAGE_DIR/User
     fi
-
+    chmod 777 -R $PACKAGE_DIR/User
 }
 
 function install_plugins() {
@@ -23,40 +23,83 @@ function install_plugins() {
         mkdir -p "$INSTALLED_PACKAGE_DIR"
     fi
     wget https://packagecontrol.io/Package%20Control.sublime-package -O "$INSTALLED_PACKAGE_DIR/Package Control.sublime-package"
+    wget https://packagecontrol.io/Package%20Control.sublime-package -O "$INSTALLED_PACKAGE_DIR/Package Control.sublime-package"
 
     #Git
-    git clone git://github.com/kemayo/sublime-text-2-git.git "$PACKAGE_DIR/Git"
-    
+    if [ !  -d "$PACKAGE_DIR/Git" ]; then
+        git clone git://github.com/kemayo/sublime-text-2-git.git "$PACKAGE_DIR/Git"
+    fi
+
     #SideBarEnhancements
-    git clone https://github.com/titoBouzout/SideBarEnhancements.git "$PACKAGE_DIR/SideBarEnhancements"
+    if [ !  -d "$PACKAGE_DIR/SideBarEnhancements" ]; then
+        git clone https://github.com/titoBouzout/SideBarEnhancements.git "$PACKAGE_DIR/SideBarEnhancements"
+    fi
 
     #BracketHighlighter
-    git clone https://github.com/facelessuser/BracketHighlighter.git "$PACKAGE_DIR/BracketHighlighter"
+    if [ !  -d "$PACKAGE_DIR/BracketHighlighter" ]; then
+        git clone https://github.com/facelessuser/BracketHighlighter.git "$PACKAGE_DIR/BracketHighlighter"
+    fi
 
     #SublimeCodeIntel
-    git clone git://github.com/SublimeCodeIntel/SublimeCodeIntel.git "$PACKAGE_DIR/SublimeCodeIntel"
-    
+     if [ !  -d "$PACKAGE_DIR/SublimeCodeIntel" ]; then
+        git clone git://github.com/SublimeCodeIntel/SublimeCodeIntel.git "$PACKAGE_DIR/SublimeCodeIntel"
+    fi
+
+
     #jQuery
-    git clone https://github.com/SublimeText/jQuery.git "$PACKAGE_DIR/jQuery"
+    if [ !  -d "$PACKAGE_DIR/jQuery" ]; then
+         git clone https://github.com/SublimeText/jQuery.git "$PACKAGE_DIR/jQuery"
+    fi
 
     #DocBlockr
-    git clone https://github.com/spadgos/sublime-jsdocs.git "$PACKAGE_DIR/DocBlockr"
+     if [ !  -d "$PACKAGE_DIR/DocBlockr" ]; then
+         git clone https://github.com/spadgos/sublime-jsdocs.git "$PACKAGE_DIR/DocBlockr"
+    fi
 
     #SublimeAllAutocomplete
-    git clone https://github.com/alienhard/SublimeAllAutocomplete "$PACKAGE_DIR/SublimeAllAutocomplete"
+     if [ !  -d "$PACKAGE_DIR/SublimeAllAutocomplete" ]; then
+         git clone https://github.com/alienhard/SublimeAllAutocomplete "$PACKAGE_DIR/SublimeAllAutocomplete"
+    fi
 
     #GitGutter
-    git clone git://github.com/jisaacks/GitGutter.git "$PACKAGE_DIR/GitGutter"
-    
+     if [ !  -d "$PACKAGE_DIR/GitGutter" ]; then
+         git clone git://github.com/jisaacks/GitGutter.git "$PACKAGE_DIR/GitGutter"
+    fi
+
     #AutoFileName
-    git clone https://github.com/BoundInCode/AutoFileName.git "$PACKAGE_DIR/AutoFileName"
+     if [ !  -d "$PACKAGE_DIR/AutoFileName" ]; then
+         git clone https://github.com/BoundInCode/AutoFileName.git "$PACKAGE_DIR/AutoFileName"
+    fi
 
     #terminal
-    git clone https://github.com/wbond/sublime_terminal.git "$PACKAGE_DIR/sublime_terminal"
+         if [ !  -d "$PACKAGE_DIR/sublime_terminal" ]; then
+         git clone https://github.com/wbond/sublime_terminal.git "$PACKAGE_DIR/sublime_terminal"
+    fi
 
     #pretty json
-    git clone https://github.com/dzhibas/SublimePrettyJson.git "$PACKAGE_DIR/SublimePrettyJson"
+    if [ !  -d "$PACKAGE_DIR/SublimePrettyJson" ]; then
+         git clone https://github.com/dzhibas/SublimePrettyJson.git "$PACKAGE_DIR/SublimePrettyJson"
+    fi
 
+    #CodeFormater
+    if [ ! -d "$PACKAGE_DIR/CodeFormatter" ]; then
+         git clone https://github.com/akalongman/sublimetext-codeformatter.git "$PACKAGE_DIR/CodeFormatter"
+    fi
+
+    #
+    # THEMES
+    
+    #Soda
+    if [ ! -d "$PACKAGE_DIR/Soda-theme" ]; then
+		git clone https://github.com/buymeasoda/soda-theme.git "$PACKAGE_DIR/Soda-theme"
+    fi
+
+    #Material
+    if [ ! -d "$PACKAGE_DIR/Material-theme" ]; then
+		https://github.com/equinusocio/material-theme.git "$PACKAGE_DIR/Material-theme"
+    fi
+
+    
 }
 
 function main() {
